6168428aaf163ff5dae2fcaec5b127a7b272c717
Lebyy <45114019+Lebyy@users.noreply.github.com>
2021-05-02 21:08:56 +0530

    Update levels.js

--- models/levels.js
+++ models/levels.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const LevelSchema = new mongoose.Schema({
+  boost: { 
+   enabledForeveryone: Boolean,
+   Boost: Number
+  }
   userID: { type: String },
   guildID: { type: String },
   xp: { type: Number, default: 0 },
